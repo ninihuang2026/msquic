113b21ba0ad572c75b01f7df4fdb278500542ed0
Masahiro Kozuka <masa.koz@kozuka.jp>
2026-05-06 12:02:29 +0900

    WIP in schannel

--- src/core/frame.c
+++ src/core/frame.c
@@ -1755,6 +1755,7 @@ QxPingFrameDecode(
     return TRUE;
 }
 
+#include <stdio.h>
 _IRQL_requires_max_(DISPATCH_LEVEL)
 BOOLEAN
 QuicFrameLog(
@@ -2556,6 +2557,7 @@ QuicFrameLog(
             PtkConnPre(Connection),
             PktRxPre(Rx),
             PacketNumber);
+        printf("QX TRANSPORT_PARAMETERS Length: %llu\n", Frame.Length);
         break;
     }
 
