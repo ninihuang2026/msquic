7686034ae6ff27245d5360ba0def4e096052c250
Masahiro Kozuka <masa.koz@kozuka.jp>
2026-05-08 10:59:07 +0900

    fix for CI

--- src/core/frame.c
+++ src/core/frame.c
@@ -1686,7 +1686,7 @@ QxTransportParametersFrameEncode(
     Buffer = Buffer + *Offset;
     Buffer = QuicVarIntEncode(QX_FRAME_TRANSPORT_PARAMETERS, Buffer);
     Buffer = QuicVarIntEncode(Frame->Length, Buffer);
-    CxPlatCopyMemory(Buffer, Frame->TP, Frame->Length);
+    CxPlatCopyMemory(Buffer, Frame->TP, (size_t)Frame->Length);
     *Offset += RequiredLength;
 
     return TRUE;
--- src/core/packet_builder.c
+++ src/core/packet_builder.c
@@ -1328,7 +1328,7 @@ QuicPacketBuilderQMuxFinalize(
     if (!CxPlatTlsEncrypt(
             QMux->TLS,
             &EncryptBuffer)) {
-        QuicConnFatalError(Connection, QUIC_STATUS_TLS_ERROR, "Encryption failure");
+        QuicConnFatalError(Connection, QUIC_STATUS_ABORTED, "Encryption failure");
         goto Exit;
     }
     Builder->Datagram->Length = (uint32_t)EncryptBuffer.DataLength;
